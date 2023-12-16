a1acfac465d04d61b03fb3e6c89fae61ca71ffe6
JackAlgera <jack.algera@live.com>
2023-12-16 10:57:02 +0100

    feat(adventofcode): 2020: finish day 12 🌟🌟

--- AdventOfCode/src/main/java/adventofcode/utils/enums/Direction.java
+++ AdventOfCode/src/main/java/adventofcode/utils/enums/Direction.java
@@ -1,5 +1,15 @@
 package adventofcode.utils.enums;
 
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST
+    NORTH, EAST, SOUTH, WEST;
+
+    public Direction rotateLeft(int turns) {
+        int totalDirections = Direction.values().length;
+        return Direction.values()[(totalDirections + this.ordinal() - turns) % totalDirections];
+    }
+
+    public Direction rotateRight(int turns) {
+        int totalDirections = Direction.values().length;
+        return Direction.values()[(totalDirections + this.ordinal() + turns) % totalDirections];
+    }
 }
