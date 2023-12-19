04f149f295e06f8a7d8fb3be103f7db575707452
JackAlgera <jack.algera@live.com>
2023-12-19 08:44:52 +0100

    feat(adventofcode): 2023: finish day 18 🌟🌟

--- AdventOfCode/src/main/java/adventofcode/utils/enums/Direction.java
+++ AdventOfCode/src/main/java/adventofcode/utils/enums/Direction.java
@@ -16,4 +16,24 @@ public enum Direction {
     public boolean isOpposite(Direction direction) {
         return this.rotateLeft(2) == direction;
     }
+
+    public static Direction fromOrientation(String s) {
+        return switch (s) {
+            case "U" -> N;
+            case "R" -> E;
+            case "D" -> S;
+            case "L" -> W;
+            default -> throw new IllegalArgumentException("Unknown orientation: " + s);
+        };
+    }
+
+    public static Direction fromDigit(int d) {
+        return switch (d) {
+            case 0 -> E;
+            case 1 -> S;
+            case 2 -> W;
+            case 3 -> N;
+            default -> throw new IllegalArgumentException("Unknown digit direction: " + d);
+        };
+    }
 }
