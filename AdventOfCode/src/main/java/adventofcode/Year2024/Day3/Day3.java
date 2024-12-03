e57dbb303ab8b245fe5a1e7814b55162df9cc8bf
Jack Algera <jack.algera@live.com>
2024-12-03 06:34:46 +0100

    feat(adventofcode): 2024: finish day 03 🌟🌟

---
+++ AdventOfCode/src/main/java/adventofcode/Year2024/Day3/Day3.java
@@ -0,0 +1,79 @@
+package adventofcode.Year2024.Day3;
+
+import adventofcode.utils.Day;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day3 extends Day<Long> {
+
+    public static void main(String[] args) throws IOException {
+        Day3 day = new Day3();
+
+        day.printPart1("sample-input-1", 161L);
+        day.printPart1("input", 168539636L);
+
+        day.printPart2("sample-input-2", 48L);
+        day.printPart2("input", 97529391L);
+    }
+
+    @Override
+    public Long part1(List<String> lines) throws IOException {
+        Pattern pattern = Pattern.compile("mul\\(\\d+,\\d+\\)");
+        return lines.stream()
+             .map(pattern::matcher)
+                .flatMap(m -> {
+                    List<String> matches = new ArrayList<>();
+                    while (m.find()) {
+                        matches.add(m.group());
+                    }
+                    return matches.stream();
+                })
+                .mapToLong(this::mul)
+                .sum();
+    }
+
+    @Override
+    public Long part2(List<String> lines) throws IOException {
+        Pattern pattern = Pattern.compile("(mul\\(\\d+,\\d+\\)|don't\\(\\)|do\\(\\))");
+        List<String> instructions = lines.stream()
+                    .map(pattern::matcher)
+                    .flatMap(m -> {
+                        List<String> matches = new ArrayList<>();
+                        while (m.find()) {
+                            matches.add(m.group());
+                        }
+                        return matches.stream();
+                    })
+                .toList();
+        long total = 0L;
+        boolean activated = true;
+        for (String line : instructions) {
+            if (line.equals("do()")) {
+                activated = true;
+                continue;
+            }
+            if (line.equals("don't()")) {
+                activated = false;
+                continue;
+            }
+            if (activated) {
+                total += mul(line);
+            }
+        }
+
+        return total;
+    }
+
+    private long mul(String s) {
+        Matcher m = Pattern.compile("\\d+").matcher(s);
+        m.find();
+        long a = Long.parseLong(m.group());
+        m.find();
+        long b = Long.parseLong(m.group());
+        return a * b;
+    }
+}
